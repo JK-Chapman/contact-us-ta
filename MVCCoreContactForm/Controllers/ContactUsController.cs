6e642b87017be35de84a8e1499e1d0bb3f340c28
Jon Chapman <jchapman@terrapincarestation.com>
2022-04-19 20:33:42 -0400

    Add VS form files.

---
+++ MVCCoreContactForm/Controllers/ContactUsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCCoreContactForm.Models;
+
+namespace MVCCoreContactForm.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactFormModel form)
+        {
+            form.postData();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MVCCoreContactForm/Models/ContactFormModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+
+namespace MVCCoreContactForm.Models
+{
+    public class ContactFormModel
+    {
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string firstName { get; set; }
+
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string lastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string message { get; set; }
+
+        public void postData()
+        {
+            using StreamWriter file = new StreamWriter("FormOutput.csv", append: true);
+            string data = $"{this.firstName}, {this.lastName}, {this.email}, {this.message}";
+
+            file.WriteLine(data);
+        }
+    }
+}
